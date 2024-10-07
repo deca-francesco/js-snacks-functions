eb38e211051b13029d91d2ac5f8e37ff979fa327
deca-francesco <francescodecarlo70@gmail.com>
2024-10-07 18:03:51 +0200

    ADD: exercise 5

--- 05 - bonus/script.js
+++ 05 - bonus/script.js
@@ -6,13 +6,38 @@ e buonasera se è sera (oltre le 17)
 */
 
 const name = 'Mario';
+const now = new Date().getHours();
 
 
 // Dichiara la funzione qui.
-
+function greetingTime(name) {
+    if (now <= 13) {
+        console.log(`Buongiorno ${name}!`);
+    } else if (now <= 17) {
+        console.log(`Buon pomeriggio ${name}!`);
+    } else if (now > 17) {
+        console.log(`Buonasera ${name}!`);
+    }
+}
 
 // Invoca la funzione qui e stampa il risultato in console
+console.log(`Ore: ${now}`);
+greetingTime(name);
+
 
 
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
+
+
+const greetingTimeArrow = (name) => {
+    if (now <= 13) {
+        console.log(`Buongiorno ${name}!`);
+    } else if (now <= 17) {
+        console.log(`Buon pomeriggio ${name}!`);
+    } else if (now > 17) {
+        console.log(`Buonasera ${name}!`);
+    }
+}
+console.log(`Ore: ${now}`);
+greetingTimeArrow(name);
